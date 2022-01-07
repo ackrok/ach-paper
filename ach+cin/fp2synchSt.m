f9c53c9c3097d5144c8d87c946408d74942dd1ff
ackrok <51799203+ackrok@users.noreply.github.com>
2022-01-07 16:23:52 -0500

    Update fp2synchSt.m

--- ach+cin/fp2synchSt.m
+++ ach+cin/fp2synchSt.m
@@ -1,16 +1,14 @@
 %% (1) Load data
-load('fullcin_Feb21_ACh+DA+PF.mat'); load('beh_wt_ACh+DA+PF.mat')
-%sub = cinwt; beh = behwt;
-sub = cinwt(find(strcmp({cinwt.rec},'IV046_rec02')));
-beh = behwt(find(strcmp({behwt.rec},sub(1).rec)));
+% load('C:\Users\Anya\Desktop\IV_LOCAL\data_comb\fullcin+beh_ACh.mat')
+sub = cinACh(find(strcmp({cinACh.rec},'IV066_rec01')));
+beh = behACh(find(strcmp({behACh.rec},sub(1).rec)));
 
 %% (2) PETH
-gen = struct; 
-gen.bin = 0.05; gen.window = [-0.025 0.025]; %CHANGE: window for PETH
-% gen.bin = 0.02; gen.window = [-0.01 0.01];
-% gen.bin = 0.01; gen.window = [-0.005 0.005];
+%bin = 0.05; window = [-0.075 0.075]; %CHANGE: window for PETH
+bin = 0.05; window = [-0.025 0.025];
+%bin = 0.01; window = [-0.005 0.005];
 
- mat = struct; % Initialize structure
+mat = struct; % Initialize structure
 h = waitbar(0, 'PETH: CIN spikes to FP peak times');
 for x = 1:length(sub)
     %% Extract spike times
@@ -20,7 +18,7 @@ for x = 1:length(sub)
     st_other = {sub(idx).st}; % Extract spike times of all other units from this recording
     fr_other = [sub(idx).fr];
     %% PETH
-    peth = getClusterPETH(st_other, st, gen); % PETH: spike times aligned to spike times
+    peth = getClusterPETH(st_other, st, bin, window); % PETH: spike times aligned to spike times
     %%
     prob = []; 
     t_0 = 1; cts0 = []; cts = {};
@@ -31,7 +29,7 @@ for x = 1:length(sub)
     end  
     %% STA
     fp = beh(1).FP{1}; Fs = 50;
-%    fp = [beh(1).vel(1); diff(movmean(beh(1).vel,10))]; Fs = 50;
+    % fp = [beh(1).vel(1); diff(movmean(beh(1).vel,10))]; Fs = 50;
     [sta_fp,sta_time] = getSTA(fp, st, Fs, [-1, 1]);
     %% Load into output structure
     mat(x).rec = sub(x).rec; 
@@ -52,27 +50,6 @@ for x = 1:length(sub)
     mat(x).cts0_rest = mat(x).cts0(:,idxSub{2}); 
     mat(x).sta_mvmt = mat(x).sta(:,idxSub{1}); 
     mat(x).sta_rest = mat(x).sta(:,idxSub{2}); 
-%     st_mvmt = extractEventST(st, beh(1).on/Fs, beh(1).off/Fs, 0); % Event times during movement
-%     st_rest = extractEventST(st, beh(1).onRest/Fs, beh(1).offRest/Fs, 0); % Event times during movement
-%     peth_mvmt = getClusterPETH(st_other, st_mvmt, gen); % PETH: spike times aligned to spike times
-%     peth_rest = getClusterPETH(st_other, st_rest, gen); % PETH: spike times aligned to spike times
-%     cts_mvmt = {}; prob_mvmt = []; cts0_mvmt = [];
-%     cts_rest = {}; prob_rest = []; cts0_rest = [];
-%     for y = 1:length(idx)
-%         cts_mvmt{y} = peth_mvmt.cts{y};
-%         cts_rest{y} = peth_rest.cts{y};
-%         prob_mvmt(:,y) = (sum(peth_mvmt.cts{y},2))./length(st_other{y}); % Probability of firing = cts/st
-%         prob_rest(:,y) = (sum(peth_rest.cts{y},2))./length(st_other{y}); % Probability of firing = cts/st
-%         cts0_mvmt(y,:) = peth_mvmt.cts{y}(t_0,:);
-%         cts0_rest(y,:) = peth_rest.cts{y}(t_0,:);
-%     end  
-%     sta_mvmt = getSTA(fp, st_mvmt, Fs, [-1, 1]); 
-%     sta_rest = getSTA(fp, st_rest, Fs, [-1, 1]);
-%     
-%     mat(x).cts_mvmt = cts_mvmt; mat(x).cts_rest = cts_rest; 
-%     mat(x).prob_mvmt = prob_mvmt; mat(x).prob_rest = prob_rest;
-%     mat(x).cts0_mvmt = cts0_mvmt; mat(x).cts0_rest = cts0_rest;
-%     mat(x).sta_mvmt = sta_mvmt; mat(x).sta_rest = sta_rest;
     %%
     waitbar(x/length(sub),h);
 end
@@ -82,21 +59,21 @@ time = peth.time;
 %% PLOT ALL UNITS: STA to synchST
 figure; plm = floor(sqrt(length(mat))); pln = ceil(length(mat)/plm);
 a = cell(2, length(mat)); % a = cell(length(mat),length(mat)); 
-clr = {'k','m','b','g'}; % clr = {'k','m','c','g'}; % %clr = {'k','r','m','b','c','g'}; %
-%clr = {'k','g'};
+clr = {'m','r','b','c','g'}; % clr = {'k','m','c','g'}; % %clr = {'k','r','m','b','c','g'}; %
+% clr = {'k','g'};
 for x = 1:length(mat)
-    b = mat(x).cts0; % CHANGE
+    b = mat(x).cts0_rest; % CHANGE
     b(b > 1) = 1; b = sum(b,1);
-%    a{1,x} = find(b == 0); a{2,x} = find(b == max(b)); 
+    % a{1,x} = find(b == 0); a{2,x} = find(b == max(b)); 
      a{1,x} = find(b == 0); a{2,x} = find(b == 1); 
      a{3,x} = find(b == 2); a{4,x} = find(b == 3);
-     %a{5,x} = find(b >= 4); %a{6,x} = find(b >= 5);
+     a{5,x} = find(b == 4); %a{6,x} = find(b >= 5);
     sp(x) = subplot(plm,pln,x); 
-    sig = mat(x).sta; % CHANGE
+    sig = mat(x).sta_rest; % CHANGE
     for y = 1:size(a,1)
         shadederrbar(sta_time, nanmean(sig(:,a{y,x}),2), SEM(sig(:,a{y,x}),2), clr{y}); hold on
     end
-    xlabel('Latency to CIN spike (s)'); ylabel('ACh (dF/F)'); grid on
+    xlabel('Latency to CIN spike (s)'); ylabel('Photometry (% dF/F)'); grid on
     title(sprintf('%s-#%d',mat(x).rec,mat(x).n),'Interpreter','none');
 end
 linkaxes(sp,'y');
